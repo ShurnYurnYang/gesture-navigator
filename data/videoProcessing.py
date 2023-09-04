88632cd6b8c89429e363a039e4678baa1cf97f2d
Sean <seany071305@gmail.com>
2023-09-04 01:27:20 -0400

    ideas for data processing techniques

---
+++ data/videoProcessing.py
@@ -0,0 +1,4 @@
+import cv2
+import imutils
+
+# basic idea: 1. gamma correction (light conditions) 2. blur (out of focus) 3. rotation (camera rotation) 4. shearing (camera placement) 5. noise 6. backgrounds
