103984fbdd5b5d0458b5db80bd2fe55a33a2d016
Sean <seany071305@gmail.com>
2023-09-04 23:31:08 -0400

    switched to nearest interpolation for max performance

--- data/segmentCap.py
+++ data/segmentCap.py
@@ -9,7 +9,7 @@ recording = False
 
 def writerStart():
     _, frame = cap.read()
-    scaled = imutils.resize(frame, 512, 512, inter=cv2.INTER_LANCZOS4)
+    scaled = imutils.resize(frame, 512, 512, inter=cv2.INTER_NEAREST)
     (h, w) = scaled.shape[:2]
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     global out 
@@ -23,7 +23,7 @@ else:
 while cap.isOpened():
     _, frame = cap.read()
 
-    scaled = imutils.resize(frame, 512, 512, inter=cv2.INTER_LANCZOS4)
+    scaled = imutils.resize(frame, 512, 512, inter=cv2.INTER_NEAREST)
 
     key = cv2.waitKey(1) & 0xFF
     if key == ord('a'):
