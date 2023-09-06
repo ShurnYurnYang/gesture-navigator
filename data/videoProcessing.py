3d0af14a0e6f48e94f0a8430ac80037b2bc1f207
Sean <seany071305@gmail.com>
2023-09-05 21:32:57 -0400

    added gaussian blur video processing

--- data/videoProcessing.py
+++ data/videoProcessing.py
@@ -14,7 +14,7 @@ def writerStart(filename, operation):
     scaled = imutils.resize(frame, 512, 512, inter=cv2.INTER_NEAREST)
     (h, w) = scaled.shape[:2]
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    out = cv2.VideoWriter(f'./videos/{filename}_gamma_{operation}.mp4', fourcc, 30.0, (w, h), True)
+    out = cv2.VideoWriter(f'./videos/{filename}_{operation}.mp4', fourcc, 30.0, (w, h), True)
     return out
 
 def gamma_correct(frame, gamma):
@@ -27,14 +27,21 @@ def gamma_correct(frame, gamma):
 
     return corrected_frame
 
+def blur_correct(frame, kernal):
+
+    blurred_frame = cv2.GaussianBlur(frame, (kernal, kernal), sigmaX=0)
+
+    return blurred_frame
 
 for video_name in list_paths:
     video_path = os.path.join(folder_path, video_name)
 
     cap = cv2.VideoCapture(video_path)
 
-    gamma_bright_out = writerStart(video_name, "bright")
-    gamma_dark_out = writerStart(video_name, "dark")
+    gamma_bright_out = writerStart(video_name, "gamma_bright")
+    gamma_dark_out = writerStart(video_name, "gamma_dark")
+
+    gaussian_blur_out = writerStart(video_name, "gaussian_blur")
 
     print(f"Reading {video_name}...\n")
 
@@ -46,6 +53,7 @@ for video_name in list_paths:
         if ret:
             gamma_bright_out.write(gamma_correct(frame, 2.0))
             gamma_dark_out.write(gamma_correct(frame, 0.5))
+            gaussian_blur_out.write(blur_correct(frame, 7))
         else:
             reading = False
 
@@ -55,5 +63,6 @@ for video_name in list_paths:
 
     gamma_bright_out.release()
     gamma_dark_out.release()
+    gaussian_blur_out.release()
 
 print("Video processing complete...")
