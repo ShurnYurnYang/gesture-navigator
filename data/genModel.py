f9b1e9e72faae6ba7e9423fb05541bd96f3c5d33
Sean <seany071305@gmail.com>
2023-09-09 01:43:51 -0400

    dataset loading complete NOTE: VERY LARGE RAM USAGE

--- data/genModel.py
+++ data/genModel.py
@@ -1,8 +1,13 @@
 import os
 import cv2
 import numpy as np
+import sys
+from keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+import tqdm
 
 dataset_dir = './data/processed_dataset'
+num_classes = 2
 
 def preprocess_frame(frame):
     frame = frame.astype('float32') / 255.0
@@ -10,8 +15,9 @@ def preprocess_frame(frame):
     return frame
 
 def loadframes(dataset_dir):
-    data = []
-    labels = []
+
+    temp_data = []
+    temp_labels = []
 
     action_labels = os.listdir(dataset_dir)
 
@@ -19,7 +25,7 @@ def loadframes(dataset_dir):
         label_dir = os.path.join(dataset_dir, label)
 
         for video_file in os.listdir(label_dir):
-            video_path = os.path.join(dataset_dir, video_file)
+            video_path = os.path.join(label_dir, video_file)
 
             cap = cv2.VideoCapture(video_path)
 
@@ -29,15 +35,40 @@ def loadframes(dataset_dir):
                 if not ret:
                     break
 
-                frame = cv2.resize(frame, (512, 512)) #possibly redundant?
+                frame = cv2.resize(frame, (224, 224)) #possibly redundant?
 
                 frame = preprocess_frame(frame)
 
-                data.append(frame)
-                labels.append(label)
+                temp_data.append(frame)
+                temp_labels.append(label)
             
             cap.release()
-    return np.array(data), np.array(labels)
+            
+            print(f"Video {video_path} processed...")
+            
+    return np.array(temp_data), np.array(temp_labels)
 
+data, labels = loadframes(dataset_dir)
+print(data.size)
+print(labels.size)
+print("Frame loading complete...")
 
+#one-hot encoding
+label_mapping = {label: i for i, label in enumerate(np.unique(labels))} #map label to integer
+labels = [label_mapping[label] for label in labels] #replace label with its integer
+labels = to_categorical(labels, num_classes=num_classes) #one hot conversion
+print("One-hot encoding complete...")
 
+frame_train, frame_val, label_train, label_val = train_test_split(data, labels, test_size=0.1, random_state=42)
+print("Data splitting complete...")
+
+#convert frame data and label sets into numpy arrays
+frame_train = np.array(frame_train)
+frame_val = np.array(frame_val)
+label_train = np.array(label_train)
+label_val = np.array(label_val)
+
+print("Training data shape:", frame_train.shape)
+print("Validation data shape:", frame_val.shape)
+print("Training labels shape:", label_train.shape)
+print("Validation labels shape:", label_val.shape)
