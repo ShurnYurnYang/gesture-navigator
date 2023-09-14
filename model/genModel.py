7816cd74561aa343051d252079f69cd82f71f027
Sean <seany071305@gmail.com>
2023-09-14 01:41:09 -0400

    failed memory optimization FIX LATER

--- model/genModel.py
+++ model/genModel.py
@@ -3,16 +3,19 @@ import os
 import cv2
 import numpy as np
 import tensorflow as tf
+import pickle
 from keras.utils import to_categorical
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from sklearn.model_selection import train_test_split
 
-print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+#Notes: preoptizimation system RAM usage ~ 6GB
 
 start_time = time.time()
 
 dataset_dir = './data/processed_dataset' #Directory to dataset | root = /gesture-navigator 
+frames_list_dir = './model/data/frames'
+labels_list_dir = './model/data/labels'
 num_classes = 2 #Number of action classes | NOTE: will increase as new data is added
 
 def preprocess_frame(frame): #Normalize pixel values (8-bit colour)
@@ -20,8 +23,8 @@ def preprocess_frame(frame): #Normalize pixel values (8-bit colour)
 
     return frame
 
-def loadframes(dataset_dir): #load frames into dataset
-
+"""def loadframes(dataset_dir): #load frames into dataset
+    
     temp_data = []
     temp_labels = []
 
@@ -55,8 +58,115 @@ def loadframes(dataset_dir): #load frames into dataset
     return np.array(temp_data), np.array(temp_labels)
 
 data, labels = loadframes(dataset_dir) #Loads temp_data and temp_labels into the data, labels numpy arrays
+
+print("Data shape:", data.shape)
+print("Data size (MB):", data.nbytes / (1024 * 1024))
+print("Labels shape:", labels.shape)
+print("Labels size (MB):", labels.nbytes / (1024 * 1024))
+"""
+
+# REWRITE THIS BY CONSTANTLY WRITING TO ONE FILE INSTEAD OF COMBINING FILES LATER
+def load_frame_batched(dataset_dir, batched): #a better way of doing this would probably be to use numpy memmap but both work
+    action_labels = os.listdir(dataset_dir)
+
+    #global temp_data 
+    temp_data = []
+    #global temp_labels 
+    temp_labels = []
+
+    counter = 0
+
+    for label in action_labels:
+        label_dir = os.path.join(dataset_dir, label)
+
+        for video_file in os.listdir(label_dir): #os.listdir() here will return an empty list of list_dir points to a file instead of to the subdirectory
+
+            counter += 1
+
+            video_path = os.path.join(label_dir, video_file)
+
+            cap = cv2.VideoCapture(video_path)
+
+            while True:
+                ret, frame = cap.read()
+
+                if not ret:
+                    break
+
+                frame = cv2.resize(frame, (128, 128)) #Scaled down from 512x512 because of memory concerns
+
+                frame = preprocess_frame(frame) #Calls to normalization
+
+                temp_data.append(frame)
+                temp_labels.append(label) #For each frame, appends the label to that frame's respective index in the labels array
+            
+            cap.release()
+            
+            print(f"Video {video_path} processed | counter is at {counter}...")
+
+            if counter % batched == 0:
+                with open('./model/data/frames/frame_list_%04i.pkl' % counter, 'wb') as frame_file:
+                    pickle.dump(temp_data, frame_file)
+
+                with open('./model/data/labels/label_list_%04i.pkl' % counter, 'wb') as label_file:
+                    pickle.dump(temp_labels, label_file)
+
+                del temp_data, temp_labels
+                #global temp_data 
+                temp_data = []
+                #global temp_labels 
+                temp_labels = []
+
+    if counter % 40 != 0:
+        with open('./model/data/frames/frame_list_%04i.pkl' % counter, 'wb') as frame_file:
+            pickle.dump(temp_data, frame_file)
+
+        with open('./model/data/labels/label_list_%04i.pkl' % counter, 'wb') as label_file:
+            pickle.dump(temp_labels, label_file)
+
+        del temp_data, temp_labels
+
+    print("Frames written to file")
+
+                
+def concat_lists(list_dir):
+    for item in os.listdir(list_dir):
+        source_file_path = os.path.join(list_dir, item)
+        destination_file_path = os.path.join(list_dir, 'master.pkl') #Fill this
+
+        with open(source_file_path, 'rb') as source_file:
+            with open(destination_file_path, 'ab') as destination_file:
+                while True:
+                    try:
+                        data = pickle.load(source_file)
+                        pickle.dump(data, destination_file)
+                    except EOFError:
+                        break
+
+def read_into_numpy(master_dir):
+    with open(master_dir, 'rb') as file:
+        loaded_array = pickle.load(file)
+        return np.array(loaded_array)
+
+
+load_frame_batched(dataset_dir, 40)
+
+concat_lists('./model/data/frames')
+
+concat_lists('./model/data/labels')
+
+data = read_into_numpy('./model/data/frames/master.pkl')
+
+labels = read_into_numpy('./model/data/labels/master.pkl')
+
+print("Data shape:", data.shape)
+print("Data size (MB):", data.nbytes / (1024 * 1024))
+print("Labels shape:", labels.shape)
+print("Labels size (MB):", labels.nbytes / (1024 * 1024))
+
 frame_loading_time = time.time()
 print(f"Frame loading completed in {(frame_loading_time - start_time):.3f} seconds...")
+
 
 #one-hot encoding
 label_mapping = {label: i for i, label in enumerate(np.unique(labels))} #map each label to an int | label : int, label : int, label : int, etc.
